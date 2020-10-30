bc170eb79face59a56361f6e024acbb420d1ae61
Orla O'Neill <orla.o'neill@finance-ni.gov.uk>
2020-10-30 19:10:02 +0000

    Reconfigure main nav functionality

--- src/js/unity-menu-script.js
+++ src/js/unity-menu-script.js
@@ -1,40 +1,19 @@
 /* eslint-disable */
 (function ($, Drupal) {
   Drupal.behaviors.nicsdruUnityMainMenu = {
-    attach: function (context, settings) {
-      $('#main-menu').find('.expanded').once('has-submenu').each(function () {
-        $(this).children('a').attr('aria-expanded', 'false');
-        $(this).children('a').attr('aria-haspopup', 'true');
-        $(this).children('a').append('<span class="fas fa-caret-down"></span>');
+    attach: function (context) {
+      $('#main-menu').find('.menu-item').once('link').each(function () {
+        $(this).children('a').attr('role', 'menuitem');
 
-        var $menuLink = $(this).children('a');
-        var clickHandler = function (e) {
-          e.preventDefault();
-          $menuLink.attr('aria-expanded', function (i, attr) {
-            return attr == 'true' ? 'false' : 'true'
-          });
-          $menuLink.parent('.expanded').toggleClass('open');
-          $(this).parent('.expanded').siblings().removeClass('open');
-          $(this).parent('.expanded').siblings().children('.menu-link').attr('aria-expanded', 'false');
-          $(this).parent('.expanded').siblings().children('ul').removeAttr('style');
-        };
-
-        $menuLink.on('click', clickHandler);
+        if ($(this).hasClass('expanded')) {
+          $(this).children('a').attr('aria-expanded', 'false');
+          $(this).children('a').attr('aria-haspopup', 'true');
+          $(this).children('a').append('<span class="fas fa-caret-down"></span>');
+        }
       });
 
-      if ($(window).width() > 766) {
-        $('#main-menu .menu-item').mouseenter(function () {
-          $(this).children('ul').css('display', 'block');
-        });
-        $('#main-menu .menu-item').mouseleave(function () {
-          var $this = $(this);
-          if (!$this.hasClass('open')) {
-            setTimeout(function () {
-              $this.children('ul').css('display', 'none');
-            }, 200);
-          }
-        });
-      }
+      var menubar = new Menubar(document.getElementById('menu-main'));
+      menubar.init();
     }
   }
 })(jQuery, Drupal);
