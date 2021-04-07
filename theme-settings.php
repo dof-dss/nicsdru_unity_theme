57ca34f3b91bb902b45f9cf0541d11c8b73f7327
omahm <hello@onemanandhismouse.com>
2021-04-07 21:28:37 +0100

    D8UN-394 ~ Code standards

--- theme-settings.php
+++ theme-settings.php
@@ -1,6 +1,16 @@
 <?php
 
-function nicsdru_unity_theme_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id = NULL) {
+/**
+ * @file
+ * Theme settings file for nicsdru_unity_theme.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function nicsdru_unity_theme_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
