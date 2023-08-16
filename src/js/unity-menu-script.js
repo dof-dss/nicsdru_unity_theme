2d174cefdf806e257b6f4abf4e1573982857d8be
DuttonMa <martin@theduttons.org>
2023-08-16 11:45:55 +0100

    syntax fix

--- src/js/unity-menu-script.js
+++ src/js/unity-menu-script.js
@@ -61,7 +61,7 @@
   }
   $(once('esc-close-submenu', $(this))).on('keydown', escHandler);
 
-  $(once('close-submenu', $(document))).click(function (event) {
+  $(once('close-submenu', document)).click(function (event) {
     var $trigger = $('#main-menu');
     if ($trigger !== event.target && !$trigger.has(event.target).length) {
       closeMenu();
